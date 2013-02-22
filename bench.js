2a0940dbfa7073b2c652b1976bd00d65433ee237
Yuriy Bogdanov <chinsay@gmail.com>
2013-02-23 00:42:38 +0200

    add support for parallel requests

--- bench.js
+++ bench.js
@@ -4,38 +4,70 @@
 // node bench.js --cpu 10 -t 57 -n 100 -c 10
 
 var opts = require('optimist')
-  .usage('Measures NodeJS eventloop performance under cuncurrent load.\nUsage: $0 --cpu 10 -t 57 -n 100 -c 10')
+  .usage([
+    'Measures NodeJS eventloop performance under cuncurrent load.',
+    'Usage: $0 --cpu 10 -t 57 -n 100 -c 10',
+    '       $0 --cpu 10 -t [50,30,40] -n 100 -c 10'
+  ].join('\n'))
   .demand(['cpu', 't', 'n', 'c'])
   .describe('cpu', '[int] Percentage of CPU time within each request. Any value between 0 and 100')
-  .describe('t', '[int] Time of the request')
+  .describe('t', '[int|array] Time of the request (if array given, runs multiple parallel reqs)')
   .describe('n', '[int] Number of requests to make')
   .describe('c', '[int] Concurrency level');
 
 var argv = opts.argv,
-    totalTime = argv.t,
+    totalTime = typeof argv.t == 'string' ? JSON.parse(argv.t) : argv.t,
     n = argv.n,
     c = argv.c,
     cpuPart = argv.cpu,
-    ioPart = 100 - cpuPart,
-    ioTime = totalTime / 100 * ioPart,
-    cpuTime = totalTime / 100 * cpuPart;
+    ioPart = 100 - cpuPart;
 
 measureRequests(n, c, function(ms, avg){
-  var xTimes = Math.round(avg / totalTime);
+  var xTimes;
+  if (totalTime instanceof Array) {
+    xTimes = Math.round(avg / calculareParallelTime(totalTime));
+  }
+  else {
+    xTimes = Math.round(avg / totalTime);
+  }
   console.log(
-    'IO/CPU = %d/%d, %dms/req | took %dms, avg %dms/req (x%d)',
+    'IO/CPU = %d/%d, %sms/req | took %dms, avg %dms/req (x%d)',
     ioPart, cpuPart, totalTime, ms, avg, xTimes
   );
 });
 
-function request(callback) {
+function requestSingle(ioTime, cpuTime, callback) {
   var reqStart = +Date.now();
   setTimeout(function(){ // I/O work
     var cpuStart = +Date.now();
     while (Date.now() - cpuStart < cpuTime); // CPU work
     callback(Date.now() - reqStart);
   }, ioTime);
-};
+}
+
+function requestParallel(totalTime, callback) {
+  var l = totalTime.length,
+      reqs = totalTime,
+      reqStart = +Date.now();
+  for (var i = 0; i < l; i++) {
+    var io = reqs[i] / 100 * ioPart,
+        cpu = reqs[i] / 100 * cpuPart;
+    requestSingle(io, cpu, function(ms){
+      --l || callback(Date.now() - reqStart);
+    });
+  }
+}
+
+function request(callback) {
+  if (totalTime instanceof Array) {
+    requestParallel(totalTime, callback);
+  }
+  else {
+    var ioTime = totalTime / 100 * ioPart,
+        cpuTime = totalTime / 100 * cpuPart;
+    requestSingle(ioTime, cpuTime, callback);
+  }
+}
 
 function measureRequests(n, c, callback) {
   var x = n,
@@ -57,4 +89,26 @@ function measureRequests(n, c, callback) {
   while (c--) {
     measure();  
   }
-};
+}
+
+function calculareParallelTime(times) {
+  // sort parallels by time
+  times.sort();
+  // calculare cpu ranges according to single threaded cpu nature
+  var cpuRanges = [], time = 0;
+  for (var i in times) {
+    var cpuTime = times[i] / 100 * cpuPart,
+        cpuStart = times[i] / 100 * ioPart,
+        cpuEnd = times[i];
+    // search for cpu overlappings
+    for (var k = 0; k < cpuRanges.length; k++) {
+      if (Math.min(cpuRanges[k][1], cpuEnd) - Math.max(cpuRanges[k][0], cpuStart) > 0) {
+        cpuStart = cpuRanges[k][1];
+        cpuEnd = cpuStart + cpuTime;
+      }
+    }
+    cpuRanges.push([cpuStart, cpuEnd]);
+    time = cpuEnd;
+  }
+  return time;
+}
